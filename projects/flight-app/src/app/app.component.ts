7aee4a71b8f67096b3f6afb9afec76883c279391
fischerf79 <fischerf79@gmail.com>
2019-07-04 13:12:46 +0200

    LAB Uebungen - bis Installing the PWA on your Desktop

--- projects/flight-app/src/app/app.component.ts
+++ projects/flight-app/src/app/app.component.ts
@@ -1,12 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {MatSnackBar} from "@angular/material";
+import {SwUpdate} from "@angular/service-worker";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'flight-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'flight-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor() { 
-  }
+
+    constructor(private snackBar: MatSnackBar,
+                private swUpdate: SwUpdate) {
+        this.setupUpdates();
+    }
+
+
+
+    private setupUpdates(): void {
+        console.log('setupUpdates');
+        const subscription = this.swUpdate.available.subscribe(u => {
+            this.swUpdate.activateUpdate().then(e => {
+                this.snackBar.open("App updated -- please reload!", "OK");
+            });
+        });
+
+        console.log('checkForUpdate');
+        this.swUpdate.checkForUpdate();
+    }
 }
 
